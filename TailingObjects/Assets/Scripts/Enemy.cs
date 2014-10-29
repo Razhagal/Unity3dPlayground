8b4032e70c798ef15700089cb5aec3b1550b738c
Hristo Petrov <hpetrovv@gmail.com>
2014-10-30 01:10:00 +0200

    First basic exercise

---
+++ TailingObjects/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    private const float DistanceToBeKept = 5f;
+
+	private void Update ()
+    {
+        this.gameObject.transform.LookAt(target);
+
+        if (Vector3.Distance(transform.position, target.position) > DistanceToBeKept)
+        {
+            transform.Translate(0f, 0f, this.speed * Time.deltaTime);            
+        }
+	}
+}
---
+++ TailingObjects/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private GameObject player;
+    public float speed = 10f;
+
+	void Start ()
+    {
+        this.player = GameObject.Find("Player");
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.Translate(0f, 0f, this.speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.Translate(0f, 0f, -this.speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.Translate(-this.speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Translate(this.speed * Time.deltaTime, 0f, 0f);
+        }
+	}
+}
